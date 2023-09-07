12399aeaba081d177adb104ad59c648b6c8eb37a
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-08 01:06:04 +0300

    fundamental process and interview

---
+++ 00_Akbank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Metin girin: ");
+        string metin = Console.ReadLine();
+
+        string duzeltilmisMetin = DuzeltMetin(metin);
+
+        Console.WriteLine("Düzeltilmiş Metin: " + duzeltilmisMetin);
+    }
+
+    static string DuzeltMetin(string metin)
+    {
+        // "." karakterlerinin pozisyonlarını bulun
+        int index = metin.IndexOf('.');
+
+        while (index != -1)
+        {
+            // "." karakterinden sonraki pozisyona boşluk ekleyin
+            metin = metin.Insert(index + 1, " ");
+
+            // Bir sonraki "." karakterini arayın
+            index = metin.IndexOf('.', index + 2);
+        }
+
+        // Birden fazla boşluğu tek bir boşluğa çevirin
+        metin = Regex.Replace(metin, @"\s+", " ");
+
+        return metin;
+    }
+}
---
+++ 00_Akbank/README.md
@@ -0,0 +1,17 @@
+# YetGen Jump & Akbank Backend Eğitim Programı 
+
+## Teknik Test && Motivasyon Süreci
+Kod, terminalden alınan bir metin üzerinde her noktadan sonra araya otomatik bir boşluk koyan bir program yazma üzerineydi. İlgili .cs uzantıda kodu inceleyebilirsiniz.
+**Örneğin:**
+Girilen metin: "aaaa.bbbbbbb.cccccc..."
+İstenilen Çıktı: "aaaa. bbbbbbb. cccccc. . ."
+
+# İlk Hafta
+
+![Yetgenekip](img/2.jpg)
+
+
+Bu benim ekibim :D
+
+### TANIŞMA TOPLANTISI
+Akışın nasıl ilerleyeceğinin, eğitimlere nasıl ulaşacağımızın, hangi yöntemleri kullanacağımızın bilgilendirilmesi yapıldı. Bu da benim birlikte projeler gerçekleştireceğim takımım. 
