4b3a99f6ea49087088c8459ae4cec4905af403af
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-14 22:20:46 +0300

    Object Oriented Programming

---
+++ 03_Akbank/YetGenAkbankJumpOOPConsole/Common/PersonBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YetGenAkbankJumpOOPConsole.Enums;
+
+namespace YetGenAkbankJumpOOPConsole.Common
+{
+    public class Teacher
+    {
+        public Guid Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int No { get; set; }
+        public Gender Gender { get; set; }
+        public DateTimeOffset RegistrationDate { get; set; }
+    }
+}
