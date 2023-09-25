1c70b5d9e47e749f5aae0de7e60e2aab6ab23b4e
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-25 19:34:59 +0300

    Calender app example

---
+++ CalendarApp/Entities/Meeting.cs
@@ -0,0 +1,21 @@
+﻿using MeetingApp.Abstract;
+using MeetingApp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarApp.Entities
+{
+    internal class Meeting : Event, INotification
+    {
+        public List<string> Guests { get; set; }
+
+        public void GetNotification()
+        {
+            foreach (var guest in Guests)
+                Console.WriteLine($"Davetli kişiler: {guest}");
+        }
+    }
+}
